dc4b806f57a167bb870c38af0d884578b1835fb0
Arjun <arjun.santhan@gmail.com>
2020-09-10 14:15:07 -0500

    Tested the Syntax errors

---
+++ Test.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+
+{
+   public static void main(String[] args)
+   {
+      System.out.println("An Emergency Broadcast");
+   }
+}
+/* 
+ * a. Change Test to test - there will be a compile error
+ * 
+ * b. Change Emergency to emergency - no error
+ * 
+ * c. Remove first quotation mark - there will be a compile-time error
+ * 
+ * d. Remove last quotation mark - there will be a compile-time error
+ * 
+ * e. Change main to man - there will be a compile-time error
+ * 
+ * f. Change println to bogus - there will be a compile-time error.
+ * 
+ * g. Remove semicolon at end of println statement - There will be a compile - time error
+ * 
+ * h. remove the last brace in the program - there will be a compile time error
+ */
