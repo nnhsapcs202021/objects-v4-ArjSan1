a96bd42aa1aca4af5337385e3c4291af61c8ca92
Arjun <arjun.santhan@gmail.com>
2020-09-10 13:44:38 -0500

    added identifiers to chapter 1 notes

--- TurtleDemo.java
+++ TurtleDemo.java
@@ -61,6 +61,46 @@ public class TurtleDemo
         
         crush.forward(25); // the crush object will move forward by 25 pixels
         
+        /*
+         * Identifiers are any combination of:
+         * letters
+         * digits (but not as first character)
+         * underscores (_)
+         * dollar signs ($)
+         * 
+         * These are the various identifiers:
+      
+         */
+        
+        int y, x2, x_y, x$;
+        
+        // This is not a valid identifier:
+        // int 2y;
+        
+        /*
+         * Identifiers are case sensitive (case matters).
+         * 
+         * These are different:
+         * 
+         */
+        Turtle squirt, SQUIRT, SqQuiRt;
+        
+        /*
+         * By convention, constant values are all uppercase. Use an underscore between words
+         * 
+         */
+        int SPEED_OF_LIGHT = 300000000; // m/s
+        
+        
+        /*
+         * By convention, most identifiers start with a lowercase letter.
+         * Subsequent words start with an uppercase letter (CamelCase)
+         */
+        
+        World pacificOcean;
+        
+        
+        
         
     }
 }
