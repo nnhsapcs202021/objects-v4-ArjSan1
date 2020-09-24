13e321e41ecbef53b0426ae67307e6e2da20eeb3
Arjun <arjun.santhan@gmail.com>
2020-09-24 15:55:32 -0500

    Fixed DieSimulator

--- DieSimulator.java
+++ DieSimulator.java
@@ -4,10 +4,9 @@ public class DieSimulator
     public static void main(String[] args)
     {Random random = new Random();
         int rand = 0;
-        while (true){
-            rand = random.nextInt(7);
-            if(rand !=0) break;
+        rand = random.nextInt(5)+1;
+         System.out.print(rand);
         }
-        System.out.print(rand);
+       
     }
-}
+
