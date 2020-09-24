8a74fcafda4bfc7cc449174548b1c0c7f35f1a1d
Arjun <arjun.santhan@gmail.com>
2020-09-24 13:40:24 -0500

    Turtle

--- TurtleLab.java
+++ TurtleLab.java
@@ -5,11 +5,12 @@ public class TurtleLab
     public static void main(String[] args)
     {
         World myWorld = new World(1000, 1000);
-        
+
         Turtle wumbo = new Turtle(myWorld);
         wumbo.setPenColor(Color.RED);
         wumbo.penDown();
         wumbo.hide();
         wumbo.forward(100);
+
     }
 }
