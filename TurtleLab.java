dea90568e9c2edd5af4e2fa3a9b5c1ea85d362b4
Arjun <arjun.santhan@gmail.com>
2020-09-29 18:30:47 -0500

    Attempted to use recursive methods for Turtle

--- TurtleLab.java
+++ TurtleLab.java
@@ -1,16 +1,26 @@
 import java.awt.Color;
+import java.util.*;
 
 public class TurtleLab
-{
-    public static void main(String[] args)
-    {
-        World myWorld = new World(1000, 1000);
 
-        Turtle wumbo = new Turtle(myWorld);
-        wumbo.setPenColor(Color.RED);
-        wumbo.penDown();
-        wumbo.hide();
-        wumbo.forward(100);
-
+{   // make turtle
+    World myWorld = new World(1920, 1080);
+    Turtle wumbo = new Turtle(myWorld);
+    public void TurtleRecursion(int sideNum, int sideLen) {
+      
+        // making a recursive polygon
+        for (int i = 0;  i <= sideNum; i++) {
+                  if (sideLen < 2){
+                    break;
+                }
+           
+                      
+            int angle = (360/sideNum);
+            wumbo.forward(sideLen);
+            wumbo.turn(angle);
+            TurtleRecursion(sideNum, sideLen-2);
+          
+        }
+        
     }
 }
