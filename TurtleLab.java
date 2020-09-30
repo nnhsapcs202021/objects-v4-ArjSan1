51c642bee48c338493819e2518b7b2aaab564031
Arjun <arjun.santhan@gmail.com>
2020-09-30 18:17:00 -0500

    Made 2 new turtles to create a spiral pattern with multiple colors. Use 3 for sideNum and 5 for sideLen for best results.

--- TurtleLab.java
+++ TurtleLab.java
@@ -3,51 +3,78 @@ import java.util.*;
 
 public class TurtleLab
 
-{   // make turtle
-    World myWorld = new World(1920, 1080);
+{   
+    World myWorld = new World(1920,1080); // make world
+    // make turtles
     Turtle wumbo = new Turtle(myWorld);
     Turtle argon = new Turtle(myWorld);
     Turtle turtle = new Turtle(myWorld);
+    Turtle t = new Turtle(myWorld);
+    Turtle u = new Turtle(myWorld);
+    
     public void TurtleRecursion(int sideNum, int sideLen) {
+        // Various methods
         wumbo.hide();
         wumbo.setPenColor(Color.BLUE);
+        wumbo.setPenWidth(3);
         argon.hide();
-        argon.setPenColor(Color.CYAN);
-        
-        // making a recursive polygon
+        argon.setPenColor(Color.RED);
+        argon.setPenWidth(3);
+        turtle.hide();
+        turtle.setPenColor(Color.GREEN);
+        turtle.setPenWidth(3);
+        t.hide();
+        t.setPenColor(Color.ORANGE);
+        t.setPenWidth(3);
+        u.hide();
+        u.setPenColor(Color.BLACK);
+        u.setPenWidth(3);
+        // Recursive Algorithm
         for (int i = 0;  i < sideNum; i++) {
-                  
-           
-                      
-            int angle = (360/sideNum);
-            wumbo.forward(sideLen);
-            wumbo.turn(angle);
-            argon.forward(sideLen);
-            argon.turn(angle);
+            int angle = (360/sideNum);       
+            t.forward(sideLen);
+            t.turn(angle);
             turtle.forward(sideLen);
             turtle.turn(angle);
+            argon.forward(sideLen);
+            argon.turn(angle);
+            wumbo.forward(sideLen);
+            wumbo.turn(angle);
+            u.forward(sideLen);
+            u.turn(angle);
         }
-           wumbo.forward(2);
-           wumbo.turn(4);
-           wumbo.forward(5);
-           argon.forward(8);
-           argon.turn(-4);
-           argon.forward(-6);
-           turtle.forward(4);
-           turtle.turn(3);
-           turtle.backward(8);
-           
-           
-           try {
-                Thread.sleep(100);
-            }
-            catch(InterruptedException ex)
-            {
-                Thread.currentThread().interrupt();
-            }
-            TurtleRecursion(sideNum, sideLen-2);
-          
+        // Methods to make turtle move
+        u.forward(1);
+        u.turn(4);
+        
+        t.forward(2);
+        t.turn(4);
         
         
-    }
+        wumbo.forward(3);
+        wumbo.turn(4);
+        
+        
+        argon.forward(4);
+        argon.turn(4);
+        
+        
+        turtle.forward(5);
+        turtle.turn(4);
+        
+        
+        // Slow down the turtles
+        
+        try {
+            Thread.sleep(100);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+        // Run the method
+        TurtleRecursion(sideNum, sideLen+2);
+    }   
+
 }
+
