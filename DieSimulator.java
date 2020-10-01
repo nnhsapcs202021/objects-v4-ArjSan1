c3c5cdebb658a6d8175f7d7dbe74523ddc0f65fc
Arjun <arjun.santhan@gmail.com>
2020-10-01 14:18:40 -0500

    New edits

--- DieSimulator.java
+++ DieSimulator.java
@@ -3,9 +3,9 @@ public class DieSimulator
 {
     public static void main(String[] args)
     {Random random = new Random();
-        int rand = 0;
-        rand = random.nextInt(5)+1;
-         System.out.print(rand);
+        int rand;
+        rand = random.nextInt(5)-1;
+         System.out.println(rand);
         }
        
     }
--- TestError.java
+++ TestError.java
@@ -10,7 +10,8 @@ public class TestError
 {
    public static void main(String[] args)
    {
-      System.out.println("An Emergency Broadcast");
+      String mis = new String ("An Emergency broadcast");
+      
    }
 }
 /* 
